d092d1fac86dd270b5bf8e93efd432770b8836dc
BurgerHead <burgerhead@Acer-Aspire-3.bilbobaggs.lan>
2025-10-05 21:46:18 -0500

    Cleaned up a lot of the aesthecics. Made a lot of calculation adjustments.

--- Python/classes/player_classes.py
+++ Python/classes/player_classes.py
@@ -25,156 +25,205 @@ class player_character:
     self.specialty_title = jclass[1]
     self.job_specialty = jclass[2]
     self.name = pf.choose_player_name(name)
-    self.hit_dice = class_profile[0]
+    self.hit_die = class_profile[0]
+    self.hit_dice = level
     self.speed = racial_profile[2]
     self.size = racial_profile[1]
 
   # Charisma attribute list.
-    self.charisma = ability_rolls[0]
-    self.charisma_mod = ((int(ability_rolls[0])-10)//2)+racial_profile[0][0]
+    self.charisma = ability_rolls[0] + racial_profile[0][0]
+    self.charisma_mod = ((int(self.charisma) - 10) // 2)
 
     if "Charisma" in class_profile[4]:
+      self.charisma_proficiency = True
       self.charisma_save = self.charisma_mod + self.proficiency_bonus
     else:
+      self.charisma_proficiency = False
       self.charisma_save = self.charisma_mod
 
     if "Deception" in skill_proficiencies:
-      self.deception = self.charisma_save + self.proficiency_bonus
+      self.deception_proficiency = True
+      self.deception = self.charisma_mod + self.proficiency_bonus
     else:
-      self.deception = self.charisma_save
+      self.deception_proficiency = False
+      self.deception = self.charisma_mod
 
     if "Intimidation" in skill_proficiencies:
-      self.intimidation = self.charisma_save + self.proficiency_bonus
+      self.intimidation_proficiency = True
+      self.intimidation = self.charisma_mod + self.proficiency_bonus
     else:
-      self.intimidation = self.charisma_save
+      self.intimidation_proficiency = False
+      self.intimidation = self.charisma_mod
 
     if "Performance" in skill_proficiencies:
-      self.performance = self.charisma_save + self.proficiency_bonus
+      self.performance_proficiency = True
+      self.performance = self.charisma_mod + self.proficiency_bonus
     else:
-      self.performance = self.charisma_save
+      self.performance_proficiency = False
+      self.performance = self.charisma_mod
 
     if "Persuasion" in skill_proficiencies:
-      self.persuasion = self.charisma_save + self.proficiency_bonus
+      self.persuasion_proficiency = True
+      self.persuasion = self.charisma_mod + self.proficiency_bonus
     else:
-      self.persuasion = self.charisma_save
+      self.persuasion_proficiency = False
+      self.persuasion = self.charisma_mod
 
   # Constitution attribute list.
-    self.constitution = ability_rolls[1]
-    self.constitution_mod = ((int(ability_rolls[1])-10)//2)+racial_profile[0][1]
-    self.hit_points = self.constitution_mod + int(self.hit_dice)
+    self.constitution = ability_rolls[1] + racial_profile[0][1]
+    self.constitution_mod = ((int(self.constitution) - 10) // 2)
+    self.hit_points = self.constitution_mod + int(self.hit_die)
 
     if "Constitution" in class_profile[4]:
+      self.constitution_proficiency = True
       self.constitution_save = self.constitution_mod + self.proficiency_bonus
     else:
+      self.constitution_proficiency = False
       self.constitution_save = self.constitution_mod
 
   # Dexterity attribute list
-    self.dexterity = ability_rolls[2]
-    self.dexterity_mod = ((int(ability_rolls[2])-10)//2)+racial_profile[0][2]
+    self.dexterity = ability_rolls[2] + racial_profile[0][2]
+    self.dexterity_mod = ((int(self.dexterity) - 10) // 2)
 
     if "Dexterity" in class_profile[4]:
+      self.dexterity_proficiency = True
       self.dexterity_save = self.dexterity_mod + self.proficiency_bonus
     else:
+      self.dexterity_proficiency = False
       self.dexterity_save = self.dexterity_mod
 
-    self.armor_class = 10 + self.dexterity_save
+    self.armor_class = 10 + self.dexterity_mod
 
     if "Acrobatics" in skill_proficiencies:
-      self.acrobatics = self.dexterity_save + self.proficiency_bonus
+      self.acrobatics_proficiency = True
+      self.acrobatics = self.dexterity_mod + self.proficiency_bonus
     else:
-      self.acrobatics = self.dexterity_save
+      self.acrobatics_proficiency = False
+      self.acrobatics = self.dexterity_mod
 
     if "Sleight of Hand" in skill_proficiencies:
-      self.sleight_of_hand = self.dexterity_save + self.proficiency_bonus
+      self.sleight_of_hand_proficiency = True
+      self.sleight_of_hand = self.dexterity_mod + self.proficiency_bonus
     else:
-      self.sleight_of_hand = self.dexterity_save
+      self.sleight_of_hand_proficiency = False
+      self.sleight_of_hand = self.dexterity_mod
 
     if "Stealth" in skill_proficiencies:
-      self.stealth = self.dexterity_save + self.proficiency_bonus
+      self.stealth_proficiency = True
+      self.stealth = self.dexterity_mod + self.proficiency_bonus
     else:
-      self.stealth = self.dexterity_save
+      self.stealth_proficiency = False
+      self.stealth = self.dexterity_mod
 
   # Intelligence attribute list
-    self.intelligence = ability_rolls[3]
-    self.intelligence_mod = ((int(ability_rolls[3])-10)//2)+racial_profile[0][3]
+    self.intelligence = ability_rolls[3] + racial_profile[0][3]
+    self.intelligence_mod = ((int(self.intelligence) - 10) // 2)
 
     if "Intelligence" in class_profile[4]:
+      self.intelligence_proficiency = True
       self.intelligence_save = self.intelligence_mod + self.proficiency_bonus
     else:
+      self.intelligence_proficiency = False
       self.intelligence_save = self.intelligence_mod
 
     if "Arcana" in skill_proficiencies:
-      self.arcana = self.intelligence_save + self.proficiency_bonus
+      self.arcana_proficiency = True
+      self.arcana = self.intelligence_mod + self.proficiency_bonus
     else:
-      self.arcana = self.intelligence_save
+      self.arcana_proficiency = False
+      self.arcana = self.intelligence_mod
 
     if "History" in skill_proficiencies:
-      self.history = self.intelligence_save + self.proficiency_bonus
+      self.history_proficiency = True
+      self.history = self.intelligence_mod + self.proficiency_bonus
     else:
-      self.history = self.intelligence_save
+      self.history_proficiency = False
+      self.history = self.intelligence_mod
 
     if "Investigation" in skill_proficiencies:
-      self.investigation = self.intelligence_save + self.proficiency_bonus
+      self.investigation_proficiency = True
+      self.investigation = self.intelligence_mod + self.proficiency_bonus
     else:
-      self.investigation = self.intelligence_save
+      self.investigation_proficiency = False
+      self.investigation = self.intelligence_mod
 
     if "Nature" in skill_proficiencies:
-      self.nature = self.intelligence_save + self.proficiency_bonus
+      self.nature_proficiency = True
+      self.nature = self.intelligence_mod + self.proficiency_bonus
     else:
-      self.nature = self.intelligence_save
+      self.nature_proficiency = False
+      self.nature = self.intelligence_mod
 
     if "Religion" in skill_proficiencies:
-      self.religion = self.intelligence_save + self.proficiency_bonus
+      self.religion_proficiency = True
+      self.religion = self.intelligence_mod + self.proficiency_bonus
     else:
-      self.religion = self.intelligence_save
+      self.religion_proficiency = False
+      self.religion = self.intelligence_mod
 
   # Strength attributes list
-    self.strength = ability_rolls[4]
-    self.strength_mod = ((int(ability_rolls[4])-10)//2)+racial_profile[0][4]
+    self.strength = ability_rolls[4] + racial_profile[0][4]
+    self.strength_mod = ((int(self.strength) - 10) // 2)
 
     if "Strength" in class_profile[4]:
+      self.strength_proficiency = True
       self.strength_save = self.strength_mod + self.proficiency_bonus
     else:
+      self.strength_proficiency = False
       self.strength_save = self.strength_mod
 
     if "Athletics" in skill_proficiencies:
-      self.athletics = self.strength_save + self.proficiency_bonus
+      self.athletics_proficiency = True
+      self.athletics = self.strength_mod + self.proficiency_bonus
     else:
-      self.athletics = self.strength_save
+      self.athletics_proficiency = False
+      self.athletics = self.strength_mod
 
   # Wisdom attribute list
-    self.wisdom = ability_rolls[5]
-    self.wisdom_mod = ((int(ability_rolls[5])-10)//2)+racial_profile[0][5]
+    self.wisdom = ability_rolls[5] + racial_profile[0][5]
+    self.wisdom_mod = ((int(self.wisdom) - 10) // 2)
 
     if "Wisdom" in class_profile[4]:
+      self.wisdom_proficiency = True
       self.wisdom_save = self.wisdom_mod + self.proficiency_bonus
     else:
+      self.wisdom_proficiency = False
       self.wisdom_save = self.wisdom_mod
 
     if "Animal Handling" in skill_proficiencies:
-      self.animal_handling = self.wisdom_save + self.proficiency_bonus
+      self.animal_handling_proficiency = True
+      self.animal_handling = self.wisdom_mod + self.proficiency_bonus
     else:
-      self.animal_handling = self.wisdom_save
+      self.animal_handling_proficiency = False
+      self.animal_handling = self.wisdom_mod
 
     if "Insight" in skill_proficiencies:
-      self.insight = self.wisdom_save + self.proficiency_bonus
+      self.insight_proficiency = True
+      self.insight = self.wisdom_mod + self.proficiency_bonus
     else:
-      self.insight = self.wisdom_save
+      self.insight_proficiency = False
+      self.insight = self.wisdom_mod
 
     if "Medicine" in skill_proficiencies:
-      self.medicine = self.wisdom_save + self.proficiency_bonus
+      self.medicine_proficiency = True
+      self.medicine = self.wisdom_mod + self.proficiency_bonus
     else:
-      self.medicine = self.wisdom_save
+      self.medicine_proficiency = False
+      self.medicine = self.wisdom_mod
 
     if "Perception" in skill_proficiencies:
-      self.perception = self.wisdom_save + self.proficiency_bonus
+      self.perception_proficiency = True
+      self.perception = self.wisdom_mod + self.proficiency_bonus
     else:
-      self.perception = self.wisdom_save
+      self.perception_proficiency = False
+      self.perception = self.wisdom_mod
 
     if "Survival" in skill_proficiencies:
-      self.survival = self.wisdom_save + self.proficiency_bonus
+      self.survival_proficiency = True
+      self.survival = self.wisdom_mod + self.proficiency_bonus
     else:
-      self.survival = self.wisdom_save
+      self.survival_proficiency = False
+      self.survival = self.wisdom_mod
 
 class ability_list:
     def __init__(self, ability, value):
--- Python/main.py
+++ Python/main.py
@@ -5,7 +5,6 @@ from classes import player_classes as pc
 from functions import player_functions as pf
 from game_tables import character_tables as ct
 
-
 os.system('clear')
 
 ability_scores = []
@@ -18,7 +17,7 @@ players = input("How many players are there? ")
 
 print()
 count = 1
-while count <= int(players):
+while int(players) >= count:
     player_name = "Player " + str(count)
     player_list.append(pc.player_character(1, 2, player_name, ct.race_list,
                                            ct.class_list))
@@ -32,39 +31,220 @@ for player in player_list:
   print("")
   print(f"_" * width)
   print(name)
-  census = f"|--------| Race: {player.race:^12} |-------| Subrace: \
-      {player.subrace:^16} |-------|"
+  print("|" + " " * int(width - 2) + "|")
+  race = player.race + " |"
+  subrace = player.subrace + " |"
+  left_padding = "|" + "-" * 2 + "|"
+  center_padding = "-" * 1 + "|"
+  right_padding = "-" * 29 + "|"
+  census = f"{left_padding} Race: {race:-<12}{center_padding} Subrace: {subrace\
+:-<16}{right_padding}"
   print(census)
-  job_class_info = f"|-| Class: {player.job_class:^11} |-| {\
-      player.specialty_title:^21}: {player.job_specialty:^27} |-|"
+  job_class = player.job_class + " |"
+  job_title = "| " + player.specialty_title
+  job_specialty = job_title + ": " + player.job_specialty + " |"
+  n = 2
+  left_padding = "|" + "-" * n + "|"
+  right_padding = "-" * int(n + 1) + "|"
+  job_class_info = f"{left_padding} Class: {job_class:-<11}-{job_specialty:-<53\
+}{right_padding}"
   print(job_class_info)
-  stats = f"|----| Armor Class: {player.armor_class:^3} |-| Hit Points: \
-{player.hit_points:^3} |-| Hit Die: d{player.hit_dice:^2d} |-| Level: \
-{player.level:^2} |----|"
+  print("|" + (" " * (width - 2))  + "|")
+  stats = f"|--| Armor Class: {player.armor_class:^3} |-| Hit Points: {player.\
+      hit_points:^3} |-| Hit Die: {player.hit_dice:2d} x d{player.\
+      hit_die:^2d} |-| Level: {player.level:>2} |-|"
   print(stats)
   print(f"|" + ("#" * (width - 2)) + "|")
-  print(f"| Cha Roll: {player.charisma:2d} |--| Mod: {player.charisma_save:2d} |")
-  print(f"| Con Roll: {player.constitution:2d} |--| Mod: {player.constitution_save:2d} |")
-  print(f"| Dex Roll: {player.dexterity:2d} |--| Mod: {player.dexterity_save:2d} |")
-  print(f"| Int Roll: {player.intelligence:2d} |--| Mod: {player.intelligence_save:2d} |")
-  print(f"| Str Roll: {player.strength:2d} |--| Mod: {player.strength_save:2d} |")
-  print(f"| Wis Roll: {player.wisdom:2d} |--| Mod: {player.wisdom_save:2d} |")
-  print(f"| {player.proficiency_bonus:2d} :Proficiency Bonus |")
-  print(f"| {player.acrobatics:2d} :Acrobatics |")
-  print(f"| {player.animal_handling:2d} :Animal Handling |")
-  print(f"| {player.arcana:2d} :Arcana |")
-  print(f"| {player.athletics:2d} :Athletics |")
-  print(f"| {player.deception:2d} :Deception |")
-  print(f"| {player.history:2d} :History |")
-  print(f"| {player.insight:2d} :Insight |")
-  print(f"| {player.intimidation:2d} :Intimidation |")
-  print(f"| {player.investigation:2d} :Investigation |")
-  print(f"| {player.medicine:2d} :Medicine |")
-  print(f"| {player.nature:2d} :Nature |")
-  print(f"| {player.perception:2d} :Perception |")
-  print(f"| {player.performance:2d} :Performance |")
-  print(f"| {player.persuasion:2d} :Persuasion |")
-  print(f"| {player.religion:2d} :Religion |")
-  print(f"| {player.sleight_of_hand:2d} :Sleight of Hand |")
-  print(f"| {player.stealth:2d} :Stealth |")
-  print(f"| {player.survival:2d} :Survival |")
+  row = f"|  Proficiency Bonus: {player.proficiency_bonus:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+  print(f"|" + (" " * (width - 2)) + "|")
+# Row for strength and wisdom
+  if player.strength_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  save = player.strength_save
+  roll = player.strength
+  row = f"| {bubble}Strength Mod: {save:>2d} ({roll:>2d})  "
+
+  if player.wisdom_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  save = player.wisdom_save
+  roll = player.wisdom
+  row = row + f"    {bubble}Wisdom Mod: {save:>2d} ({roll:>2d})"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for athletics and animal handling
+  if player.athletics_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = f"|   {bubble}Athletics: {player.athletics:2d}  "
+  if player.animal_handling_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = row + " " * 12 + f"{bubble}Animal Handling: {player.animal_handling:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for insight
+  if player.insight_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = "|" + " " * 32 + f"{bubble}Insight: {player.insight:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for dexterity and medicine
+  if player.dexterity_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  save = player.dexterity_save
+  roll = player.dexterity
+  row = f"| {bubble}Dexerity Mod: {save:>2d} ({roll:>2d})  "
+  if player.medicine_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = row + " " * 6 + f"{bubble}Medicine: {player.medicine:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for acrobatics and perception
+  if player.acrobatics_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = f"|   {bubble}Acrobatics: {player.acrobatics:2d}"
+  if player.perception_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = row + " " * 13 + f"{bubble}Perception: {player.perception:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for sleight of hand and survival
+  if player.sleight_of_hand_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = f"|   {bubble}Sleight of Hand: {player.sleight_of_hand:2d}   "
+  if player.survival_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = row + " " * 5 + f"{bubble}Survival: {player.survival:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for stealth
+  if player.stealth_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = f"|   {bubble}Stealth: {player.stealth:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for charisma
+  if player.charisma_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  save = player.charisma_save
+  roll = player.charisma
+  row = "|" + " " * 30 + f"{bubble}Charisma Mod: {save:>2d} ({roll:<2d})"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for intelligence and deception
+  if player.intelligence_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  save = player.intelligence_save
+  roll = player.intelligence
+  row = f"| {bubble}Intelligence Mod: {save:>2d} ({roll:>2d})"
+  if player.deception_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = row + " " * 4 + f"{bubble}Deception: {player.deception:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for arcana and intimidation
+  if player.arcana_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = f"|   {bubble}Arcana: {player.arcana:2d}"
+  if player.intimidation_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = row + " " * 17 + f"{bubble}Intimidation: {player.intimidation:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for history and performance
+  if player.history_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = f"|   {bubble}History: {player.history:2d}"
+  if player.performance_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = row + " " * 16 + f"{bubble}Performance: {player.performance:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for investigation and persuasion
+  if player.investigation_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = f"|   {bubble}Investigation: {player.investigation:2d}"
+  if player.persuasion_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = row + " " * 10 + f"{bubble}Persuasion: {player.persuasion:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for nature
+  if player.nature_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = f"|   {bubble}Nature: {player.nature:2d}"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
+# Row for religion and constitution
+  if player.religion_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  row = f"|   {bubble}Religion: {player.religion:2d}"
+  if player.constitution_proficiency:
+    bubble = "\u25CF "
+  else:
+    bubble = "\u25CB "
+  save = player.constitution_save
+  roll = player.constitution
+  row = row + " " * 13 + f"{bubble}Constitution Mod: {save:>2d} ({roll:>2d})"
+  row = row + (" " * (width - len(row) - 1)) + "|"
+  print(row)
+
